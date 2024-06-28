65cee92764e47958b73596127b878a54811122db
cromizeco <cromizeco@gmail.com>
2024-06-28 16:08:11 +0530

    Update footer.tsx

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -9,7 +9,7 @@ export const Footer = () => {
 		<footer className="border-t py-20 dark:border-neutral-800 ">
 			<Container className="px-5">
 				{PUBLICATION_LOGO ? (
-					<div className="mb-20 flex w-full flex-row justify-center">
+					<div className="mb-10 flex w-full flex-row justify-center">
 						<Link
 							href={'/'}
 							aria-label={`${publication.title} home page`}
@@ -19,11 +19,19 @@ export const Footer = () => {
 						</Link>
 					</div>
 				) : (
-					<p className="mb-20 text-center text-xl font-semibold text-slate-900 dark:text-slate-50 md:text-4xl">
+					<p className="mb-10 text-center text-xl font-semibold text-slate-900 dark:text-slate-50 md:text-4xl">
 						{publication.title}
 					</p>
 				)}
-				<div className="flex justify-center text-slate-600 dark:text-neutral-300">
+				<div className="flex flex-col items-center text-slate-600 dark:text-neutral-300">
+					<div className="mb-5">
+						<Link href="https://cromize.com/blog" className="mr-5 text-blue-600 hover:underline">
+							Blogs
+						</Link>
+						<Link href="https://cromize.com/" className="text-blue-600 hover:underline">
+							Conversion Rate Optimization
+						</Link>
+					</div>
 					<p>&copy; 2024 Cromize</p>
 				</div>
 			</Container>
