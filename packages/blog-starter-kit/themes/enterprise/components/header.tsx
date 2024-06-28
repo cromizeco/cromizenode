3b9257ed640f15bb9de2cc1ecca742e6d0bde803
unknown <hamzasaleem3335@gmail.com>
2024-06-28 14:15:58 +0500

    some updates

--- packages/blog-starter-kit/themes/enterprise/components/header.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/header.tsx
@@ -98,8 +98,10 @@ export const Header = () => {
 					</div>
 				</div>
 				<div className="col-span-2 flex flex-row items-center justify-end gap-5 text-slate-300 lg:col-span-3">
-					<nav className="hidden lg:block">{navList}</nav>
-					<Button href={baseUrl} as="a" type="primary" label="Book a demo" />
+					{/* <nav className="hidden lg:block">{navList}</nav> */}
+					<Button href={baseUrl} as="a" type="primary" label="Conversion Rate Optimization" />
+					<Button href={baseUrl} as="a" type="primary" label="Blogs" />
+					<Button href={baseUrl} as="a" type="primary" label="Book a Call" />
 				</div>
 			</Container>
 			<div className="mt-5 flex justify-center lg:hidden">
